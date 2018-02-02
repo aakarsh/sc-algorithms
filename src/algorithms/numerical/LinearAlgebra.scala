5f5a508504eb0c40adff74515b1271c230512825
aakarsh nair <aakarsh.nair@gmail.com>
2018-02-01 16:20:57 -0800

    Adding regressions

--- src/algorithms/numerical/LinearAlgebra.scala
+++ src/algorithms/numerical/LinearAlgebra.scala
@@ -19,6 +19,80 @@ object LinearAlgebra {
   
   def eq[T <:Double](x: Array[T], y:Array[T]) :  Boolean =  x.zip(y).map(pair.equal).reduce(_ && _)
   
+  object Matrix {
+    
+    def polynomial_regression(xy:List[(Double, Double)]): List[Double] = {      
+      val n:Int = xy.size
+      
+      val y = new Array[Double](n)      
+      val m = new Array[Array[Double]](n)
+      
+      var l = xy
+      for(i <- 0 until n) {
+        val (x_i,y_i) = l.head
+        
+        for(j<- 0 until n) {
+          if(j == 0)
+            m(i) = new Array[Double](n)
+            
+          m(i)(j) = Math.pow(x_i,j)
+        }
+        
+        y(i) = y_i
+        l = l.tail
+      }
+      
+      val M = new Matrix(m)      
+      M.solve(y).toList
+    }
+    
+    def regression(xy: List[(List[Double], Double)]) : List[Double] = {
+      val n = xy.size  // assuming square.
+      
+      val y = new Array[Double](n)
+      val m = new Array[Array[Double]](n)
+      
+      var i = 0
+      for( (xs , f) <- xy) {
+        
+        var j = 0
+        
+        for(x <- xs) {
+          if(j == 0)
+            m(i) = new Array[Double](n)
+          m(i)(j) = x
+          
+          j+=1
+        }
+        
+        y(i) = f
+        i+=1
+      }
+      
+      val M = new Matrix(m)
+      M.solve(y).toList
+    }
+    
+     def I(size:Int):Matrix = identity(size)
+     
+     def identity(size:Int):Matrix = {
+       val v = new Array[Double](size)
+       for (i <- 0 until size) {
+         v(i) = 1
+       }
+       diagonal(v)
+     }
+     
+     def diagonal(diagonal:Array[Double]) : Matrix = {
+        val m = new Array[Array[Double]](diagonal.length)
+        for(i <- 0 until diagonal.length) {
+          m(i) = new Array[Double](diagonal.length)
+          m(i)(i) = diagonal(i)
+        }     
+        m
+     }       
+  }
+  
   implicit class Matrix(m: Array[Array[Double]]) {
   
   val nrows = m.length
@@ -45,24 +119,34 @@ object LinearAlgebra {
   def scale_row( r:Int , scale:Double) : Unit =  
     m(r) = m(r).map(v => scale * v)
   
+  def solve(b:Array[Double]) : Array[Double] = {
+    var retval = Array[Double](b.size)
+    forward_substitute(b) match {
+      case (u:Matrix,y: Array[Double]) => {
+        retval = u.backward_substitute(y)
+      }
+    }
+    retval
+  }
   /**
    * Convert Ax = b into upper triangular form Ux = y
    * 
    * Assumes A is invertible. 
    */
   def forward_substitute(b: Array[Double]): (Matrix, Array[Double]) = {
+    
     val U = m.clone()
     val y = b.clone()
     
     for(pivot <- 0 until nrows) {
       val p = pivot
       
-      // scale row of pivot row to make the pivot entry equal to 1
+      // Scale row of pivot row to make the pivot entry equal to 1
       val scale = 1/U(p)(p)
-      y(p) /= scale 
-      U(p) = U(p) map ( _ /scale )
+      y(p) *= scale 
+      U(p) = U(p) map ( _ * scale )
       
-      // eliminate pivot from future rows below pivot entry
+      // Eliminate pivot from future rows below pivot entry
       for(r <- (p + 1) until nrows) {
         val scale = -U(r)(p) // entry in current row of pivot column
         // eliminate in y
@@ -78,6 +162,8 @@ object LinearAlgebra {
   def backward_substitute(y: Array[Double]): Array[Double] = {
     val x = y.clone
     for( pivot <- (nrows - 1) to 0 by -1 ) { // go back by row.
+        //println("pivot:"+pivot)
+        //println("x:\n"+x.toList)
         for( r <- (0 to (pivot-1))) {
           val p = pivot
           x(r) = x(r) - (m(r)(p) / m(p)(p)) * x(p)
@@ -86,12 +172,35 @@ object LinearAlgebra {
     x
   }
   
+  var lu_cache: Option[Array[Array[Double]]] = None
+  
+  def lu_factorization : Matrix = {
+    if(lu_cache.isEmpty) {
+       val a = m.clone()
+       for( p <- 0 until ncols) {
+         for( r <- (p + 1) until nrows) {
+           
+           val scale = -a(r)(p)/a(p)(p) // does not account f
+           a(r)(p)   = -scale // set r > b, lower part of matrix
+           
+           for( c <- (p + 1) until ncols ) {
+             a(r)(c) += scale * a(p)(c)
+           }           
+         }
+       }
+       
+       lu_cache = Some(a)       
+    }
+    lu_cache.get
+  }
+  
   /**
    * Unoptimized multiply two dimensional matrix:
    *  { A \in R^(m*n) with vector v \in R^n  returning vector in R^m }
    */
   def multiply(x: Array[Double]) : Array[Double] = 
     m map { r =>  dot(x,r) }  
+  
   
   override def toString : String  = {
       val nrows = m.length
@@ -106,16 +215,14 @@ object LinearAlgebra {
       }          
       s.toString
     }
-  }
+  } 
 
   /**
-   * 
+   *  
    */
   def main(args: Array[String]) = {    
     
-    val I:Matrix = Array(Array(1.0 , 0.0   , 0.0),
-                         Array(0.0 , 1.0   , 0.0),
-                         Array(0.0 , 0.0   , 1.0))
+    val I: Matrix = Matrix.identity(3)
                   
     val x = Array(1.0, 2.0, 3.0)
     
@@ -134,6 +241,25 @@ object LinearAlgebra {
     println("After forward:")
     println(U)
     println(y.toList)
+    
+    println("LU :")
+    
+    println(Matrix.diagonal(Array(1.0,2.0,3.0)).lu_factorization)
+    
+    val v: Matrix = Array(Array(1.0, 1.0, -2.0),
+                          Array(0.0, 1.0, -1.0),
+                          Array(3.0, -1.0, 1.0))
+
+    println(v.lu_factorization)
+    
+    val r = Matrix.regression(List((List(1,2),1) , 
+                                   (List(2,3),2) ,
+                                   (List(3,4),3)))
+                        
+    println(r)
+    
+    val pr = Matrix.polynomial_regression(List((-1,1),(0,-1),(2,7)))
+    println(pr)
     
   }
   
